b82445e91dc596c1bc9fce8a844a5f1713a25c79
Wink Saville <wink@saville.com>
2022-08-19 21:23:46 -0700

    feat: Starting to add code coverage
    Using -Cinstrument-coverage, grcov and then adding gen-profraw,
    gen-html and gen-lcov to xtask.
    
    
    Also added sub as well as fixing the add/Cargo.toml name to
    "add".

--- sub/src/main.rs
+++ sub/src/main.rs
@@ -1,9 +1,9 @@
-fn add(a: i32, b: i32) -> i32 {
+fn sub(a: i32, b: i32) -> i32 {
     a + b
 }
 
 fn main() {
-    println!("add: 1 + 2 = {}", add(1, 2));
+    println!("sub: 1 - 2 = {}", sub(1, 2));
 }
 
 #[cfg(test)]
@@ -12,12 +12,12 @@ mod test {
 
     #[test]
     fn test_succeeds() {
-        assert_eq!(add(1, 1), 2);
+        assert_eq!(sub(1, 1), 2);
     }
 
     #[test]
     fn test_negatives() {
-        assert_eq!(add(-1, -2), -3);
+        assert_eq!(sub(-1, -2), -3);
     }
 
     // To run this test:
@@ -26,6 +26,6 @@ mod test {
     #[ignore]
     #[test]
     fn test_fails() {
-        assert_eq!(add(1, 1), 1);
+        assert_eq!(sub(1, 1), 1);
     }
 }
--- xtask/src/main.rs
+++ xtask/src/main.rs
@@ -16,6 +16,9 @@ fn main() -> Result<(), DynError> {
         Some("test") => cargo_cmd("test", &remaining_args)?,
         Some("clippy") => cargo_cmd("clippy", &remaining_args)?,
         Some("pre-commit") => pre_commit(&remaining_args)?,
+        Some("gen-profraw") => gen_profraw()?,
+        Some("gen-html") => gen_html()?,
+        Some("gen-lcov") => gen_lcov()?,
         _ => print_help(),
     }
     Ok(())
@@ -35,6 +38,121 @@ fn pre_commit(remaining_args: &Vec<String>) -> Result<(), DynError> {
     cargo_cmd_prj_root("test", remaining_args)?;
     cargo_cmd_prj_root("clippy", remaining_args)?;
     cargo_cmd_prj_root("fmt", remaining_args)?;
+
+    Ok(())
+}
+
+fn gen_profraw() -> Result<(), DynError> {
+    eprintln!("Create profraw data");
+
+    let coverage_dir = project_coverage_root()?;
+
+    let status = Command::new("cargo")
+        .env("CARGO_INCREMENTAL", "0")
+        .env("RUSTFLAGS", "-Cinstrument-coverage")
+        .env("TMPDIR", coverage_dir)
+        .env("LLVM_PROFILE_FILE", "%t/cargo-test-%p-%m.profraw")
+        .arg("test")
+        .args(["-p", "sub", "-p", "add"])
+        //.args(remaining_args)
+        .status()?;
+
+    if !status.success() {
+        Err("cargo test with code-coverage {remaining_args:?} Failed")?;
+    }
+
+    Ok(())
+}
+
+fn gen_html() -> Result<(), DynError> {
+    let pb = project_root().join("coverage").join("html");
+    let output_path = pb.to_string_lossy();
+    eprintln!("Create {output_path}");
+
+    let pb = project_root().join("target").join("debug").join("deps");
+    let debug_deps_dir_full_path = pb.to_string_lossy();
+
+    let pb = Path::new("/home/wink/prgs/rust/clones/grcov/target/debug/grcov");
+    let grcov = pb.to_string_lossy().to_string();
+    //let status = Command::new("grcov")
+    println!("gen_html: grcov={grcov}");
+    let status = Command::new(grcov)
+        .current_dir(project_root())
+        .args([
+            ".",
+            "--binary-path",
+            &debug_deps_dir_full_path,
+            "--branch",
+            "--ignore-not-existing",
+            "--source-dir",
+            ".",
+            // Ignore path releative to --source-dir
+            //"--ignore", // Not sure what this does?
+            //"../*",
+            //"--ignore", // Not sure what this does?
+            //"/*",
+            "--ignore",
+            "xtask/*",
+            "--ignore",
+            "*/src/tests/*",
+            "--output-type",
+            "html",
+            "--output-path",
+            &output_path,
+        ])
+        //.args(remaining_args)
+        .status()?;
+
+    if !status.success() {
+        Err(format!("Creating {output_path} Failed"))?;
+    }
+
+    Ok(())
+}
+
+fn gen_lcov() -> Result<(), DynError> {
+    let pb = project_root().join("coverage").join("tests.lcov");
+    let output_path = pb.to_string_lossy();
+    eprintln!("Create {output_path}");
+
+    //let pb = project_root().join("coverage");
+    //let coverage_dir_full_path = pb.to_string_lossy();
+    let pb = project_root().join("target").join("debug").join("deps");
+    let debug_deps_dir_full_path = pb.to_string_lossy();
+
+    let pb = Path::new("/home/wink/prgs/rust/clones/grcov/target/debug/grcov");
+    let grcov = pb.to_string_lossy().to_string();
+    //let status = Command::new("grcov")
+    let status = Command::new(&grcov)
+        .current_dir(project_root())
+        .args([
+            ".", //&coverage_dir_full_path,
+            "--binary-path",
+            &debug_deps_dir_full_path,
+            "--branch",
+            "--ignore-not-existing",
+            "--source-dir",
+            ".",
+            // Ignore path releative to --source-dir
+            //"--ignore", // Not sure what this does?
+            //"../*",
+            //"--ignore", // Not sure what this does?
+            //"/*",
+            "--ignore",
+            "xtask/*",
+            "--ignore",
+            "*/src/tests/*",
+            "--output-type",
+            "lcov",
+            "--output-path",
+            &output_path,
+        ])
+        //.args(remaining_args)
+        .status()?;
+
+    if !status.success() {
+        Err(format!("Creating {output_path} Failed"))?;
+    }
 
     Ok(())
 }
@@ -76,6 +194,16 @@ fn cargo_string() -> String {
             exit(2);
         }
     }
+}
+
+fn project_coverage_root() -> Result<String, DynError> {
+    let pb = project_root().join("coverage");
+    let coverage_dir = match pb.to_str() {
+        Some(dir) => dir,
+        None => return Err("Unable to create coverage dir".into()),
+    };
+
+    Ok(coverage_dir.to_owned())
 }
 
 fn project_root() -> PathBuf {
